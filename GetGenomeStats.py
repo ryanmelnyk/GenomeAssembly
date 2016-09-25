4811079bfc95636cf2bd87f9495de62fa14200a9
Ryan A. Melnyk <schmelnyk@gmail.com>
2016-09-25 16:07:10 -0700

    added length/coverage cutoffs for GetGenomeStats.py

--- GetGenomeStats.py
+++ GetGenomeStats.py
@@ -16,10 +16,13 @@ For parsing contig files and producing statistics on N50, coverage, etc. Expects
 fasta-formatted file with a SPAdes format header.
 	''')
 	parser.add_argument('fastafile', type=str,help='relative path to fasta file containing contigs')
+	parser.add_argument('len_cutoff', type=int,help='cutoff for length of contigs')
+	parser.add_argument('cov_cutoff', type=float,help='cutoff for coverage of contigs')
 	parser.add_argument('prefix',type=str,help='prefix for output files')
 	return parser.parse_args()
 
-def parse_seqs(fastafile):
+def parse_seqs(fastafile,prefix,len_cutoff,cov_cutoff):
+	o = open(prefix+".filteredcontigs.fna",'w')
 	contigcount = 0
 	leng = {}
 	cov = {}
@@ -31,17 +34,24 @@ def parse_seqs(fastafile):
 		else:
 			leng[contigname] = math.log(int(vals[3]),10)
 			cov[contigname] = float(vals[5])
+		if leng[contigname] > len_cutoff and cov[contigname] > cov_cutoff:
+			SeqIO.write(seq,o,'fasta')
 	d = {'log10length':pd.Series(leng),'coverage':pd.Series(cov)}
+	o.close()
 	return pd.DataFrame(d)
 
 def main():
 	args = parse_args()
 	fastafile = os.path.abspath(args.fastafile)
-	prefix = args.prefix
+	len_cutoff = args.len_cutoff
+	cov_cutoff = args.cov_cutoff
+	prefix = os.path.abspath(args.prefix)
 
-	df = parse_seqs(fastafile)
+	df = parse_seqs(fastafile,prefix,len_cutoff,cov_cutoff)
 	fig = plt.figure()
 	df.plot(x="coverage",y="log10length",kind="scatter")
+	plt.axhline(math.log(len_cutoff, 10))
+	plt.axvline(cov_cutoff)
 	plt.savefig('{}.png'.format(prefix),dpi=300)
 
 
